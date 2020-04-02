817a2efe15faf545ecaf440ac48597e55be366e3
Beatrix Lee <blsr3@academy.cin.ufpe.br>
2020-04-02 19:22:05 -0300

    Primeiro playground desenvolvidodentro do programa de desenvolvimento para iOS - Apple Developer Academy

--- TheCult.playground/Contents.swift
+++ TheCult.playground/Contents.swift
@@ -5,21 +5,29 @@ import PlaygroundSupport
 
 
 let cfURL = Bundle.main.url(forResource: "Quicksand-Bold", withExtension: "ttf")! as CFURL
-  CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
+CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
 
 
-class MyViewController : UIViewController {
+class FirstViewController : UIViewController {
     
-    @IBAction func touchedButton() {
-        print("oi")
+    @IBAction func touchedButtonPlay() {
+        print("tocou botão jogar")
+        let vc = SecondViewController(screenType: .mac, isPortrait: true)
+        navigationController?.pushViewController(vc, animated: true)
+        
+
+    }
+    
+    @IBAction func touchedButtonGallery() {
+        print("tocou botão galeria")
     }
     
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-        
+        navigationController?.navigationBar.isHidden = true
         let imgBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: 1440, height: 900))
-               imgBackground.image = UIImage(imageLiteralResourceName: "Background")
+        imgBackground.image = UIImage(imageLiteralResourceName: "Background")
         
         
         let imgView = UIImageView(frame: CGRect(x: 118, y: 126, width: 1204, height: 660))
@@ -31,10 +39,10 @@ class MyViewController : UIViewController {
         buttonPlay.frame = CGRect(x: 312, y: 380, width: 780, height: 140)
         
         let imagePlay = UIImage(named: "Play")!
-              buttonPlay.setImage(imagePlay, for: .normal)
+        buttonPlay.setImage(imagePlay, for: .normal)
         
         
-        buttonPlay.addTarget(self, action: #selector(MyViewController.touchedButton), for: .touchUpInside)
+        buttonPlay.addTarget(self, action: #selector(FirstViewController.touchedButtonPlay), for: .touchUpInside)
         
         
         
@@ -42,17 +50,21 @@ class MyViewController : UIViewController {
         buttonGallery.frame = CGRect(x: 334, y: 510, width: 759, height: 140)
         let imageGallery = UIImage(named: "gallery")!
         buttonGallery.setImage(imageGallery, for: .normal)
-    
         
-        buttonGallery.addTarget(self, action: #selector(MyViewController.touchedButton), for: .touchUpInside)
+        
+        buttonGallery.addTarget(self, action: #selector(FirstViewController.touchedButtonGallery), for: .touchUpInside)
         
         let theCult = UILabel()
         theCult.frame = CGRect(x: 520, y: 220, width: 650, height: 75)
         theCult.text = "The Cult"
         theCult.textColor = .black
-    
+        
         let font = UIFont(name: "Quicksand-Bold", size: 100)
         theCult.font = font
+        
+        
+       
+        
         
         view.addSubview(imgBackground)
         view.addSubview(imgView)
@@ -66,11 +78,81 @@ class MyViewController : UIViewController {
     }
 }
 
+class SecondViewController: UIViewController {
+    
+    @IBAction func touchedButtonEasy() {
+        print("tocou botão facil")
+    }
+    
+    @IBAction func touchedButtonMedium() {
+        print("tocou botão medio")
+    }
+    
+    @IBAction func touchedButtonHard() {
+           print("tocou botão dificil")
+       }
+    
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        
+        let imgBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: 1440, height: 900))
+               imgBackground.image = UIImage(imageLiteralResourceName: "Background")
+        
+        let imgView = UIImageView(frame: CGRect(x: 118, y: 251, width: 1193, height: 415))
+               imgView.image = UIImage(imageLiteralResourceName: "Rectangle 10")
+        
+        let dificuldade = UILabel()
+        dificuldade.frame = CGRect(x: 390, y: 320, width: 928, height: 107)
+        dificuldade.text = "Defina a dificuldade"
+        dificuldade.textColor = .black
+        
+        let font = UIFont(name: "Quicksand-Bold", size: 70)
+        dificuldade.font = font
+        
+        let buttonEasy = UIButton()
+               buttonEasy.frame = CGRect(x: 179, y: 456, width: 340, height: 117)
+               
+               let imageEasy = UIImage(named: "Facil")!
+               buttonEasy.setImage(imageEasy, for: .normal)
+               
+               buttonEasy.addTarget(self, action: #selector(SecondViewController.touchedButtonEasy), for: .touchUpInside)
+        
+        let buttonMedium = UIButton()
+        buttonMedium.frame = CGRect(x: 552, y: 454, width: 340, height: 117)
+        
+        let imageMedium = UIImage(named: "Medio")!
+        buttonMedium.setImage(imageMedium, for: .normal)
+        
+        buttonMedium.addTarget(self, action: #selector(SecondViewController.touchedButtonMedium), for: .touchUpInside)
+        
+        let buttonHard = UIButton()
+        buttonHard.frame = CGRect(x: 923, y: 454, width: 340, height: 117)
+        
+        let imageHard = UIImage(named: "Dificil")!
+        buttonHard.setImage(imageHard, for: .normal)
+        
+        buttonHard.addTarget(self, action: #selector(SecondViewController.touchedButtonHard), for: .touchUpInside)
+        
+        view.addSubview(imgBackground)
+        view.addSubview(imgView)
+        view.addSubview(dificuldade)
+        view.addSubview(buttonEasy)
+        view.addSubview(buttonMedium)
+        view.addSubview(buttonHard)
+        self.view = view
+    }
+}
 // Present the view controller in the Live View window
 
 
-let viewController = MyViewController()
-let vc = MyViewController(screenType: .mac , isPortrait: true)
-PlaygroundPage.current.liveView = vc.scale(to: 0.4)
+let viewController = FirstViewController()
+let vc = FirstViewController(screenType: .mac , isPortrait: true)
+let navigationController = UINavigationController(screenType: .mac , isPortrait: true)
+navigationController.pushViewController(vc, animated: true)
+navigationController.navigationBar.isHidden = true
+PlaygroundPage.current.liveView = navigationController.scale(to: 0.4)
+
 
 
